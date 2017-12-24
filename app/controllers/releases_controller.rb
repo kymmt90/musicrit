024f0852bc0b7500aa837d876c8a65a45bf017a3
Kohei Yamamoto <kymmt90@gmail.com>
2017-12-24 18:46:09 +0900

    Create releases by paramters from the form

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -5,6 +5,22 @@ class ReleasesController < ApplicationController
 
   def new
     @musician = Musician.find(params[:musician_id])
-    @release = @musician.releases.new
+  end
+
+  def create
+    @musician = Musician.find(params[:musician_id])
+    @release = @musician.releases.build(release_params)
+    if @release.save
+      redirect_to musician_release_path(@musician, @release), notice: "#{@release.title}を登録しました"
+    else
+      flash.now[:error] = '登録できませんでした'
+      render :new
+    end
+  end
+
+  private
+
+  def release_params
+    params.require(:release).permit(:title, :released_on, :description)
   end
 end
--- spec/system/releases_spec.rb
+++ spec/system/releases_spec.rb
@@ -26,5 +26,47 @@ RSpec.describe 'Releases', type: :system do
       expect(page).to have_field '説明'
       expect(page).to have_button '登録する'
     end
+
+    context 'when submitting valid data' do
+      before { @release_attributes = attributes_for(:release) }
+
+      it 'creates new release' do
+        visit new_musician_release_path(@musician)
+        fill_in 'タイトル', with: @release_attributes[:title]
+        fill_in 'リリース日', with: @release_attributes[:released_on]
+        fill_in '説明', with: @release_attributes[:description]
+
+        expect {
+          click_button '登録する'
+        }.to change(Release, :count).by(1)
+
+        expect(current_path).to eq musician_release_path(@musician, Release.first)
+        expect(page).to have_content '登録しました'
+      end
+    end
+
+    context 'when submitting invalid data' do
+      before do
+        @release_attributes = attributes_for(:release)
+        @release_attributes[:released_on] = 'XXXX-12-01'
+      end
+
+      it 're-render the form' do
+        visit new_musician_release_path(@musician)
+        fill_in 'タイトル', with: @release_attributes[:title]
+        fill_in 'リリース日', with: @release_attributes[:released_on]
+        fill_in '説明', with: @release_attributes[:description]
+
+        expect {
+          click_button '登録する'
+        }.not_to change(Release, :count)
+
+        expect(page).to have_content '登録できませんでした'
+        expect(page).to have_field 'タイトル'
+        expect(page).to have_field 'リリース日'
+        expect(page).to have_field '説明'
+        expect(page).to have_button '登録する'
+      end
+    end
   end
 end
