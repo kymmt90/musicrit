a91b046faa5264fbcfad7ad39f80f1ea3b4bdbe5
Kohei Yamamoto <kymmt90@gmail.com>
2017-12-31 19:58:04 +0900

    Use vue.esm.js as an alias of vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const sharedConfig = require('./shared')
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
+environment.config.merge(sharedConfig)
 module.exports = environment
