af6f3aecbc1d336c167bc0cd9650f9230acce492
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-01 16:52:42 +0900

    Delete tracks

--- app/models/release.rb
+++ app/models/release.rb
@@ -14,12 +14,22 @@ class Release < ApplicationRecord
   end
 
   def update_tracks!(params)
-    params[:tracks].each_with_index do |param, index|
-      if track = tracks.find(param[:id])
-        track.update!(title: param[:title], disc_number: 1, track_number: index + 1)
-      else
-        tracks.create!(title: param[:title], disc_number: 1, track_number: index + 1)
+    transaction do
+      track_ids_to_destroy(params[:tracks]).each { |id| tracks.find(id).destroy! }
+      params[:tracks].each_with_index do |param, index|
+        begin
+          track = tracks.find(param[:id])
+          track.update!(title: param[:title], disc_number: 1, track_number: index + 1)
+        rescue ActiveRecord::RecordNotFound
+          tracks.create!(title: param[:title], disc_number: 1, track_number: index + 1)
+        end
       end
     end
+  end
+
+  private
+
+  def track_ids_to_destroy(tracks_params)
+    tracks.map(&:id) - tracks_params.map { |t| t[:id].to_i }
   end
 end
