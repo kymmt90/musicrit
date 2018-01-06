abe1a16d9620eae65b9e2366cbd3daf2bda1bc1e
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-06 16:09:07 +0900

    Add genres

---
+++ db/migrate/20180106070152_create_genres.rb
@@ -0,0 +1,12 @@
+class CreateGenres < ActiveRecord::Migration[5.2]
+  def change
+    create_table :genres do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+
+      t.timestamps
+    end
+
+    add_index :genres, :name, unique: true
+  end
+end
