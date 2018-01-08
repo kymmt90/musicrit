72dc6b79b8bb78d576a3c45fa8a0fc4103123483
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-08 21:26:13 +0900

    Set up letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'musicians#index'
+
+  devise_for :users
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   resources :genres
   resources :musicians do
