084583dccac0136bfda5f30251e42781505694b9
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-13 01:33:09 +0900

    Set up OmniAuth settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  devise :confirmable, :database_authenticatable, :recoverable, :registerable, :rememberable, :trackable, :validatable
+  devise :confirmable, :database_authenticatable, :recoverable, :registerable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: %i(twitter)
 
   has_many :authentications, dependent: :destroy
 end
