11709424016a0b8b4cb0b267847935b61dbe6b7a
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-13 21:40:59 +0900

    Not remove OmniAuth data in the session manually
    Because Devise removes all the data starting with "devise." from the session whenever a user signs in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def create
-    super do |user|
-      session.delete('devise.omniauth_data') if user.persisted?
-    end
-  end
-
   private
 
   def clean_up_password(user)
