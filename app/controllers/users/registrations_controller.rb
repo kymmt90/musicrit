7e21fe4526a696a9f3716c4b601051231a08bab4
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-13 22:06:51 +0900

    Comment the reason of overriding clean_up_passwords

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   private
 
+  # not clean up passwords when using OAuth authentications because passwords are hidden in the form
   def clean_up_passwords(user)
     super unless user.authentications.present?
   end
