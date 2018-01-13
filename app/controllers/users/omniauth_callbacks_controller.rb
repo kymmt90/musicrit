00d3bd65f5ae758251aab099ecae447df58defff
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-13 20:09:12 +0900

    Not use abbreviation names for Active Record instances to avoid misunderstandings

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    auth = Authentication.from_omniauth(auth_hash)
+    authentication = Authentication.from_omniauth(auth_hash)
 
     if user_signed_in?
-      conenct_with(auth)
+      conenct_with(authentication)
     else
-      sign_up_or_login_with(auth)
+      sign_up_or_login_with(authentication)
     end
   end
 
@@ -15,19 +15,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     request.env['omniauth.auth']
   end
 
-  def connect_with(auth)
-    if auth.update(user: current_user)
-      flash[:notice] = "#{auth.provider}のアカウントと連携しました"
+  def connect_with(authentication)
+    if authentication.update(user: current_user)
+      flash[:notice] = "#{authentication.provider}のアカウントと連携しました"
     else
       flash[:alert] = '連携に失敗しました'
     end
     redirect_to root_path
   end
 
-  def sign_up_or_login_with(auth)
-    if auth.user
-      sign_in_and_redirect auth.user, event: :authentication
-      set_flash_message(:notice, :success, kind: auth.provider)
+  def sign_up_or_login_with(authentication)
+    if authentication.user
+      sign_in_and_redirect authentication.user, event: :authentication
+      set_flash_message(:notice, :success, kind: authentication.provider)
     else
       session['devise.omniauth_data'] = auth_hash
       redirect_to new_user_registration_path
