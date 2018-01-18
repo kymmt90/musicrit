a2a677f178a47b187109aa539415cab1c0d85a12
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-19 01:49:38 +0900

    Test signing in and connecting with external services

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,46 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  describe 'new_user_registration_path' do
-    context 'when the user sign up with external service authentications' do
+  before { Rails.application.env_config['devise.mapping'] = Devise.mappings[:user] }
+
+  describe 'edit_user_registration_path' do
+    context 'when the user has already signed up with email' do
       before do
-        Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
+        email = attributes_for(:user)[:email]
+        OmniAuth.config.add_mock(:facebook, { provider: 'facebook', uid: rand(10000), info: { email: email }})
+        Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
+
+        user = create(:user, email: email, confirmed_at: Time.current)
+        sign_in user
       end
 
-      context 'when using Facebook' do
-        before do
-          email = attributes_for(:user)[:email]
-          OmniAuth.config.add_mock(:facebook, { provider: 'facebook', uid: rand(10000), info: { email: email }})
-          Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
-        end
+      it 'connects the user and Facebook authentication' do
+        visit edit_user_registration_path
 
-        it 'creates new registration with Facebook authentication' do
-          visit new_user_registration_path
-          click_link 'Sign in with Facebook'
+        expect {
+          click_link 'Connect with Facebook'
+        }.to change(Authentication, :count).by(1)
+        expect(Authentication.first.provider).to eq 'facebook'
 
-          expect {
-            click_button 'Sign up'
-          }.to change(Authentication, :count).by(1).and change(User, :count).by(1)
-          expect(Authentication.first.provider).to eq 'facebook'
-        end
+        expect(current_path).to eq root_path
+        expect(page).to have_content '連携しました'
+      end
+    end
+  end
+
+  describe 'new_user_registration_path' do
+    context 'when using Facebook' do
+      before do
+        email = attributes_for(:user)[:email]
+        OmniAuth.config.add_mock(:facebook, { provider: 'facebook', uid: rand(10000), info: { email: email }})
+        Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
       end
 
-      context 'when using Twitter' do
-        before do
-          OmniAuth.config.add_mock(:twitter, { provider: 'twitter', uid: rand(10000) })
-          Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
-        end
+      it 'creates new registration with Facebook authentication' do
+        visit new_user_registration_path
+        click_link 'Sign in with Facebook'
 
-        it 'creates new registration with Twitter authentication' do
-          visit new_user_registration_path
-          click_link 'Sign in with Twitter'
-          fill_in 'Email', with: attributes_for(:user)[:email]
+        expect {
+          click_button 'Sign up'
+        }.to change(Authentication, :count).by(1).and change(User, :count).by(1)
+        expect(Authentication.first.provider).to eq 'facebook'
+      end
+    end
 
-          expect {
-            click_button 'Sign up'
-          }.to change(Authentication, :count).by(1).and change(User, :count).by(1)
-          expect(Authentication.first.provider).to eq 'twitter'
-        end
+    context 'when using Twitter' do
+      before do
+        OmniAuth.config.add_mock(:twitter, { provider: 'twitter', uid: rand(10000) })
+        Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+      end
+
+      it 'creates new registration with Twitter authentication' do
+        visit new_user_registration_path
+        click_link 'Sign in with Twitter'
+        fill_in 'Email', with: attributes_for(:user)[:email]
+
+        expect {
+          click_button 'Sign up'
+        }.to change(Authentication, :count).by(1).and change(User, :count).by(1)
+        expect(Authentication.first.provider).to eq 'twitter'
+      end
+    end
+  end
+
+  describe 'new_user_session_path' do
+    context 'when the user has already signed up with Facebook authentication' do
+      before do
+        email = attributes_for(:user)[:email]
+        OmniAuth.config.add_mock(:facebook, { provider: 'facebook', uid: rand(10000), info: { email: email }})
+        Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
+
+        visit new_user_registration_path
+        click_link 'Sign in with Facebook'
+        click_button 'Sign up'
+        sign_out User.first
+      end
+
+      it 'signs in with Facebook authentication' do
+        visit new_user_session_path
+        click_link 'Sign in with Facebook'
+
+        expect(current_path).to eq root_path
+        expect(page).to have_content 'facebook'
       end
     end
   end
