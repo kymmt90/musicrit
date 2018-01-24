33b404b39618eda15e01a4fbe85a8f08aef452be
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-24 22:40:56 +0900

    Update musician reviews by submitting from the form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_musician, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_musician, only: [:new, :create, :edit, :update]
+  before_action :set_review, only: [:edit, :update]
 
   def index
     @user = User.includes(reviews: [:musician]).find(params[:user_id])
@@ -20,6 +21,18 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to musician_path(@musician), notice: "#{@musician.name}のレビューを更新しました"
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
   private
 
   def review_params
@@ -28,5 +41,9 @@ class ReviewsController < ApplicationController
 
   def set_musician
     @musician = Musician.find(params[:musician_id])
+  end
+
+  def set_review
+    @review = @musician.reviews.find(params[:id])
   end
 end
--- spec/system/reviews_spec.rb
+++ spec/system/reviews_spec.rb
@@ -60,6 +60,67 @@ RSpec.describe 'Reviews', type: :system do
     end
   end
 
+  describe 'edit_musician_review_path' do
+    before do
+      @user = create(:user, confirmed_at: Time.current)
+      @review = create(:review, :for_musician, user: @user)
+      @musician = @review.musician
+    end
+
+    context 'when not signing in' do
+      before { @user = create(:user, confirmed_at: Time.current) }
+
+      it 'redirects to the log in form' do
+        visit edit_musician_review_path(@musician, @review)
+
+        expect(current_path).to eq new_user_session_path
+      end
+    end
+
+    context 'when signing in' do
+      before { sign_in @user }
+
+      it 'shows the form' do
+        visit edit_musician_review_path(@musician, @review)
+
+        expect(page).to have_content @musician.name
+        expect(page).to have_field 'レビュー', with: @review.body
+        expect(page).to have_button '更新する'
+      end
+
+      context 'when submitting valid data' do
+        before { @review_attributes = attributes_for(:review) }
+
+        it 'updates the musician review' do
+          visit edit_musician_review_path(@musician, @review)
+          fill_in 'レビュー', with: @review_attributes[:body]
+
+          expect {
+            click_button '更新する'
+            @review.reload
+          }.to change(@review, :body)
+
+          expect(current_path).to eq musician_path(@musician)
+          expect(page).to have_content 'レビューを更新しました'
+        end
+      end
+
+      context 'when submitting invalid data' do
+        it 're-render the form' do
+          visit edit_musician_review_path(@musician, @review)
+          fill_in 'レビュー', with: ''
+
+          expect {
+            click_button '更新する'
+          }.not_to change(@review, :body)
+
+          expect(page).to have_content '更新できませんでした'
+          expect(page).to have_field 'レビュー'
+        end
+      end
+    end
+  end
+
   describe 'user_reviews_path' do
     context 'when not user review exists' do
       before do
