6e0bb3ace97f56b4a4c58222f74e5ba781354c02
Kohei Yamamoto <kymmt90@gmail.com>
2018-01-28 15:06:06 +0900

    Show reviews and the link to writing reviews page in track detail pages

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,6 +2,9 @@ class TracksController < ApplicationController
   def show
     musician = Musician.find(params[:musician_id])
     release = musician.releases.find(params[:release_id])
-    @track = release.tracks.find(params[:id])
+    @track = release.tracks.includes(reviews: :user).find(params[:id])
+    if current_user
+      @review = current_user.reviews.find_by(reviewable: @track)
+    end
   end
 end
